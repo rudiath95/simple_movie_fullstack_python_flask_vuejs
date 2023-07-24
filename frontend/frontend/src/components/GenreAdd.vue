786456516c87be0b5e542dd8ba1209a5f0e3c32b
rudiath95 <rudiath.r@gmail.com>
2023-07-24 14:19:12 +0700

    bug fix

--- frontend/frontend/src/components/GenreAdd.vue
+++ frontend/frontend/src/components/GenreAdd.vue
@@ -1,66 +1,63 @@
 <template>
-    <div class="p-4">
-      <form @submit.prevent="addGenre">
-        <div class="mb-4 relative ">
-          <input
-            id="genreName"
-            v-model="newGenreName"
-            type="text"
-            name="genreName"
-            placeholder="Genre Name"
-            class="mt-1 px-4 py-2 w-full border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            required
-          />
-          <button
-            type="button"
-            class="absolute top-1 right-0 mr-2 px-4 py-2 text-gray-500 font-medium"
-            @click="cancelAdd"
-          >
-            X
-          </button>
-        </div>
-        <div class="flex justify-end">
-          
-          <button
-            type="submit"
-            class="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 rounded-lg focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            @click="addGenre"
-            >
-            Add Genre
-          </button>
-        </div>
-      </form>
-    </div>
-  </template>
-  
+  <div class="p-4">
+    <form @submit.prevent="addGenre">
+      <div class="mb-4 relative">
+        <input
+          id="genreName"
+          v-model="newGenreName"
+          type="text"
+          name="genreName"
+          placeholder="Genre Name"
+          class="mt-1 px-4 py-2 w-full border border-gray-300 rounded-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          required
+        />
+        <button
+          type="button"
+          class="absolute top-1 right-0 mr-2 px-4 py-2 text-gray-500 font-medium"
+          @click="cancelAdd"
+        >
+          X
+        </button>
+      </div>
+      <div class="flex justify-end">
+        <button
+          type="submit"
+          class="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 rounded-lg focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        >
+          Add Genre
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
 <script>
 import axios from 'axios';
 
 export default {
-data() {
+  data() {
     return {
-    newGenreName: '',
+      newGenreName: '',
     };
-},
-methods: {
+  },
+  methods: {
     addGenre() {
-    const genreData = { name: this.newGenreName };
-    axios
+      const genreData = { name: this.newGenreName };
+      axios
         .post('http://localhost:5000/genre', genreData)
         .then(response => {
-        console.log('Genre added successfully:', response.data);
-        this.newGenreName = '';
+          console.log('Genre added successfully:', response.data);
+          this.newGenreName = '';
+          this.$emit('genre-added');
         })
         .catch(error => {
-        console.error('Error adding genre:', error);
+          console.error('Error adding genre:', error);
         });
-        this.$emit('genre-added');
     },
     cancelAdd() {
-    this.newGenreName = '';
-    this.$emit('cancelled');
+      this.newGenreName = '';
+      this.$emit('cancelled');
     },
-},
+  },
 };
 </script>
-
